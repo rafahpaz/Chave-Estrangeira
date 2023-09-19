d7acc4f709b722aa48aebffa43b475c67515cb1e
Rafael Paz <99267530+rafahpaz@users.noreply.github.com>
2023-09-19 09:14:14 -0300

    Stranger Key
    Feito pae

---
+++ demo/src/main/java/com/example/cadastroveiculos/controller/VeiculoController.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.cadastroveiculos.controller;
+
+/**
+ *
+ * @author rafael_k_paz
+ */
+public class VeiculoController {
+    
+}
